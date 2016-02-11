f057782f9c23dff98a70789510f557fa399696f4
Danilo Bargen <mail@dbrgn.ch>
2016-02-11 07:59:39 +0100

    Add outro

--- process.py
+++ process.py
@@ -21,41 +21,59 @@ def process(item):
     audio = item['audio']
     video = item['video']
     offset = (item['video_offset']['start'], item['video_offset']['end'])
+    duration = offset[1] - offset[0]
     print('--> Using audio "%s"...' % audio)
     print('--> Using video "%s"...' % video)
-    intro_duration = 3
+    intro_outro_duration = 3
     framerate = 25
     command = [
         'ffmpeg',
-        # Input files
-        '-itsoffset', str(intro_duration),
+
+        # Input 0: Intro
+        '-loop', '1',
+        '-i', str(item['intro']),
+
+        # Input 1: Talk
+        '-itsoffset', str(intro_outro_duration),
         '-ss', str(offset[0]),
         '-t', str(offset[1]),
         '-i', video,
+
+        # Input 2: Outro
+        '-itsoffset', str(intro_outro_duration + duration),
         '-loop', '1',
-        '-i', str(item['intro']),
-        '-itsoffset', str(intro_duration),
+        '-i', str(item['outro']),
+
+        # Input 3: Audio
+        '-itsoffset', str(intro_outro_duration),
         '-i', audio,
+
+        # Input channel mapping
+        '-map', '[v]',
+        '-map', '3:a:0',
+
         # Codecs
         '-codec:v', 'h264',
-        '-codec:a', 'aac',
+        '-codec:a', 'aac', '-strict', '-2',
+
         # Bitrate and framerate
         '-b:v', '8000k',
         '-b:a', '384k',
         '-r:v', str(framerate),
+
         # Filter
-        '-filter_complex', '[1:v] fade=out:%s:%s:alpha=1 [intro];' \
-                           % (framerate * intro_duration, framerate) +
-                           '[0:v] hqdn3d [talk];' + \
-                           '[talk][intro] overlay [v]',
-        # Input channel mapping
-        '-map', '[v]',
-        '-map', '2:a:0',
-        #'-filter_complex', 'amix=inputs=2:duration=first',
-        # Other options
-        '-strict', '-2',
-        # Duration of video
-        '-t', str(offset[1] - offset[0]),
+        '-filter_complex', '[0:v] fade=out:%s:%s:alpha=1 [intro];' \
+                           % (framerate * intro_outro_duration, framerate) +
+                           '[2:v] fade=in:0:%s:alpha=1 [outro];' \
+                           % (framerate + intro_outro_duration) +
+                           '[1:v] hqdn3d [talk];' + \
+                           '[talk][intro] overlay [tmp];' + \
+                           '[tmp][outro] overlay [v]',
+        # '-filter_complex', 'amix=inputs=2:duration=first',
+
+        # Total duration
+        '-t', str(duration + intro_outro_duration * 2),
+
         # Output file
         item['output'],
     ]
